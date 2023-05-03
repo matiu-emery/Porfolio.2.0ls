65a112c943e36437c0545c540e8102b3e46f5dfd
matiu-emery <your.name@example.commatiu.emery@gmail.com>
2023-05-04 11:36:44 +1200

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { HashRooter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
+    <HashRooter>
     <App />
+    </HashRooter>
   </React.StrictMode>,
   document.getElementById('root')
 );
